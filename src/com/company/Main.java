3c8cef3ccd1e1f73882a741b1b5bdc16fff19c94
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-04-22 23:01:02 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float subject1 = 65;
+        float subject2 = 98;
+        float subject3 = 45;
+        float subject4 = 43;
+        float subject5 = 46;
+        float subject6 = 47;
+        float cgpa = (subject1+subject2+subject3+subject4+subject5+subject6)/60;
+        System.out.println(cgpa);
+	// write your code here
+    }
+}
